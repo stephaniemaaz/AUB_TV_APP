7c05a8f4e761888c031125d4954903be49338a46
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-04 11:39:56 +0200

    Video Player Added

--- app/src/main/java/com/example/myapplication/VideoPlayActivity.java
+++ app/src/main/java/com/example/myapplication/VideoPlayActivity.java
@@ -1,15 +1,25 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import androidx.fragment.app.FragmentActivity;
+import android.content.Intent;
 import android.os.Bundle;
 
-public class VideoPlayActivity extends Activity {
+public class VideoPlayActivity extends FragmentActivity {
+
+    public static Video video;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_video_play);
+
+        Intent intent = getIntent();
+        video = (Video) intent.getSerializableExtra("video");
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, new VideoPlayFragment())
+                    .commit();
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/VideoPlayFragment.java
+++ app/src/main/java/com/example/myapplication/VideoPlayFragment.java
@@ -1,110 +1,60 @@
 package com.example.myapplication;
 
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.fragment.app.Fragment;
+import androidx.leanback.app.VideoSupportFragment;
+import androidx.leanback.app.VideoSupportFragmentGlueHost;
+import androidx.leanback.media.MediaPlayerAdapter;
+import androidx.leanback.media.PlaybackTransportControlGlue;
+import androidx.leanback.widget.PlaybackControlsRow;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+public class VideoPlayFragment extends VideoSupportFragment {
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link VideoPlayFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link VideoPlayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class VideoPlayFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public VideoPlayFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment VideoPlayFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static VideoPlayFragment newInstance(String param1, String param2) {
-        VideoPlayFragment fragment = new VideoPlayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private static ProgressDialog progressDialog;
+    private PlaybackTransportControlGlue<MediaPlayerAdapter> mTransportControlGlue;
+    private static final String TAG = "VideoPlayFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        final Video video = VideoPlayActivity.video;
+
+        progressDialog = ProgressDialog.show(getActivity(), "", "Loading...", true);
+        progressDialog.setCancelable(true);
+
+        try {
+
+            VideoSupportFragmentGlueHost glueHost =
+                    new VideoSupportFragmentGlueHost(VideoPlayFragment.this);
+
+            MediaPlayerAdapter mediaPlayerAdapter = new MediaPlayerAdapter(getActivity());
+            mediaPlayerAdapter.setRepeatAction(PlaybackControlsRow.RepeatAction.INDEX_NONE);
+
+            mTransportControlGlue = new PlaybackTransportControlGlue<>(getActivity(), mediaPlayerAdapter);
+            mTransportControlGlue.setHost(glueHost);
+            mTransportControlGlue.setTitle(video.getTitle());
+            mTransportControlGlue.setSubtitle(video.getDescription());
+            mTransportControlGlue.playWhenPrepared();
+
+            mediaPlayerAdapter.setDataSource(video.getVideoUri());
+            progressDialog.dismiss();
+
+        } catch (Exception e) {
+            Log.i("````error````: ", e.getMessage());
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_video_play, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    public void onPause() {
+        super.onPause();
+        if (mTransportControlGlue != null) {
+            mTransportControlGlue.pause();
         }
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
+//
 }
