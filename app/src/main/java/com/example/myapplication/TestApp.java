a663077701dfaddad7357e3baa6f271b13bad803
Mohammed Ezzedine <mhmd.ezdn@gmail.com>
2020-03-06 19:01:42 +0200

    Simplifying Code

--- app/src/main/java/com/example/myapplication/TestApp.java
+++ app/src/main/java/com/example/myapplication/TestApp.java
@@ -2,101 +2,66 @@ package com.example.myapplication;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.os.Build;
+import android.util.Log;
+import android.widget.Toast;
 
-import com.example.myapplication.vimeonetworking.NetworkingLogger;
-import com.example.myapplication.vimeonetworking.TestAccountStore;
-import com.example.myapplication.vimeonetworking.NetworkingLogger;
-import com.example.myapplication.vimeonetworking.TestAccountStore;
 import com.vimeo.networking.Configuration;
-import com.vimeo.networking.Vimeo.LogLevel;
 import com.vimeo.networking.VimeoClient;
+import com.vimeo.networking.callbacks.AuthCallback;
+import com.vimeo.networking.model.error.VimeoError;
 
-/**
- * Created by kylevenn on 1/27/16.
- */
 public class TestApp extends Application {
 
     private static final String SCOPE = "private public create edit delete interact";
 
-    private static final boolean IS_DEBUG_BUILD = false;
-    // Switch to true to see how access token auth works.
-    private static final boolean ACCESS_TOKEN_PROVIDED = true;
+    private static final String TAG = "TestApp";
+
+    private VimeoClient mApiClient;
 
     private static Context sContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i(TAG, "onCreate");
 
         sContext = this;
-        AccountPreferenceManager.initializeInstance(sContext);
 
-        // <editor-fold desc="Vimeo API Library Initialization">
-        Configuration.Builder configBuilder;
-        // This check is just as for the example. In practice, you'd use one technique or the other.
-        if (ACCESS_TOKEN_PROVIDED) {
-            configBuilder = getAccessTokenBuilder();
-        } else {
-            configBuilder = getClientIdAndClientSecretBuilder();
-        }
-        if (IS_DEBUG_BUILD) {
-            // Disable cert pinning if debugging (so we can intercept packets)
-            configBuilder.enableCertPinning(false);
-            configBuilder.setLogLevel(LogLevel.VERBOSE);
-        }
-        configBuilder
-                .setCacheDirectory(this.getCacheDir())
-                .setUserAgentString(getUserAgentString(this))
-                .setDebugLogger(new NetworkingLogger());
+        Configuration.Builder configBuilder = getClientIdAndClientSecretBuilder();
         VimeoClient.initialize(configBuilder.build());
-        // </editor-fold>
-    }
-
-    public Configuration.Builder getAccessTokenBuilder() {
-        // The values file is left out of git, so you'll have to provide your own access token
-        String accessToken = "XXXXXXXXXX3fb1";
-        return new Configuration.Builder(accessToken);
+        mApiClient = VimeoClient.getInstance();
+//        authenticateWithClientCredentials();
     }
 
     public Configuration.Builder getClientIdAndClientSecretBuilder() {
-        // The values file is left out of git, so you'll have to provide your own id and secret
         String clientId = "dc62dc72e8c7cf4d0b7bb025d41e2a7649ddc0a4";
         String clientSecret = "SRbzt7mLfbA0l5aEV8lXINE54nRGkSp8S2i+wOp9paOqQsEKkwC5MtdwFPHiVqX7DTCooPVWAxPVjG8/pjjPbl+7LNTXTuSDit1oZGtCjkBYFqahb95J4pAHzEumdNhs";
-        String codeGrantRedirectUri = getString(R.string.deeplink_redirect_scheme) + "://" +
-                                      getString(R.string.deeplink_redirect_host);
-        TestAccountStore testAccountStore = new TestAccountStore(this.getApplicationContext());
-        Configuration.Builder configBuilder =
-                new Configuration.Builder(clientId, clientSecret, SCOPE);
-        configBuilder
-                // Used for oauth flow
-                .setCodeGrantRedirectUri(codeGrantRedirectUri);
-
-        return configBuilder;
+        return new Configuration.Builder(clientId, clientSecret, SCOPE);
     }
 
-    public static String getUserAgentString(Context context) {
-        String packageName = context.getPackageName();
+//    private void authenticateWithClientCredentials() {
+//        VimeoClient.getInstance().authorizeWithClientCredentialsGrant(new AuthCallback() {
+//            @Override
+//            public void success() {
+//                String accessToken = VimeoClient.getInstance().getVimeoAccount().getAccessToken();
+//                Log.i(TAG, "get success.");
+////                toast("Client Credentials Authorization Success with Access Token: " + accessToken);
+//            }
+//
+//            @Override
+//            public void failure(VimeoError error) {
+//                String errorMessage = error.getDeveloperMessage();
+//                Log.i(TAG, error.getErrorMessage());
+////                toast("Client Credentials Authorization Failure: " + errorMessage);
+//            }
+//        });
+//    }
 
-        String version = "unknown";
-        try {
-            PackageInfo pInfo = context.getPackageManager().getPackageInfo(packageName, 0);
-            version = pInfo.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            System.out.println("Unable to get packageInfo: " + e.getMessage());
-        }
+    public VimeoClient getmApiClient() {
+        return mApiClient;
+    }
 
-        String deviceManufacturer = Build.MANUFACTURER;
-        String deviceModel = Build.MODEL;
-        String deviceBrand = Build.BRAND;
-
-        String versionString = Build.VERSION.RELEASE;
-        String versionSDKString = String.valueOf(Build.VERSION.SDK_INT);
-
-        return packageName + " (" + deviceManufacturer + ", " + deviceModel + ", " + deviceBrand +
-               ", " + "Android " + versionString + "/" + versionSDKString + " Version " + version +
-               ")";
+    private void toast(String string) {
+        Toast.makeText(getBaseContext(), string, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/myapplication/VideoPlayFragment.java
+++ app/src/main/java/com/example/myapplication/VideoPlayFragment.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,6 +21,7 @@ import androidx.leanback.widget.PlaybackControlsRow;
 
 import com.vimeo.networking.VimeoClient;
 import com.vimeo.networking.callbacks.AuthCallback;
+import com.vimeo.networking.callbacks.ModelCallback;
 import com.vimeo.networking.callbacks.VimeoCallback;
 import com.vimeo.networking.model.User;
 import com.vimeo.networking.model.VideoList;
@@ -35,12 +37,9 @@ public class VideoPlayFragment extends VideoSupportFragment {
     public static final String STAFF_PICKS_VIDEO_URI = "/channels/927/videos"; // 927 == staffpicks
 
     // todo: last thing done: commented the following line
-//    private final VimeoClient mApiClient = VimeoClient.getInstance();
+    private VimeoClient mApiClient;
     private ProgressDialog mProgressDialog;
 
-    private TextView mRequestOutputTv;
-
-    private TextView mStaffPicksRequestTime;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -48,8 +47,25 @@ public class VideoPlayFragment extends VideoSupportFragment {
         super.onCreate(savedInstanceState);
         final Video video = VideoPlayActivity.video;
 
-//        TestApp d = new TestApp();
-//        d.onCreate();
+        TestApp d = new TestApp();
+        d.onCreate();
+        mApiClient = d.getmApiClient();
+
+        String uri = video.getVideoUri().toString();
+        mApiClient.getInstance().fetchNetworkContent(uri, new ModelCallback<Video>(Video.class) {
+            @Override
+            public void success(Video video) {
+                Log.i(TAG, "get success.");
+                // use the video
+            }
+
+            @Override
+            public void failure(VimeoError error) {
+                Log.i(TAG, error.getErrorMessage());
+                // voice the error
+            }
+        });
+
 
         mProgressDialog = ProgressDialog.show(getActivity(), "", "Loading...", true);
         mProgressDialog.setCancelable(true);
@@ -81,35 +97,6 @@ public class VideoPlayFragment extends VideoSupportFragment {
 
     }
 
-//    @Override
-//    public void onClick(View v) {
-////        switch (v.getId()) {
-////            case R.id.staff_picks_gson_btn:
-////                fetchStaffPicksWithGson();
-////                break;
-////            case R.id.staff_picks_moshi_btn:
-////                fetchStaffPicksWithMoshi();
-////                break;
-////            case R.id.account_type_btn:
-////                fetchAccountType();
-////                break;
-////            case R.id.logout_btn:
-////                logout();
-////                break;
-////            case R.id.code_grant_btn:
-////            case R.id.fab:
-////                toast("Authenticate on Web");
-////                goToWebForCodeGrantAuth();
-////                break;
-////        }
-//    }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -119,143 +106,6 @@ public class VideoPlayFragment extends VideoSupportFragment {
 
         return super.onOptionsItemSelected(item);
     }
-
-    // </editor-fold>
-
-    // <editor-fold desc="Requests">
-
-    private void fetchStaffPicksWithGson() {
-        final long initialTime = System.currentTimeMillis();
-        mProgressDialog.show();
-//        mApiClient.getContent(STAFF_PICKS_VIDEO_URI, CacheControl.FORCE_NETWORK, GetRequestCaller.VIDEO_LIST, null, null, null, new VimeoCallback<VideoList>() {
-//            @Override
-//            public void success(VideoList videoList) {
-//                final long finalTime = System.currentTimeMillis();
-//                final long totalLoadTime = finalTime - initialTime;
-//
-//                if (videoList != null && videoList.getData() != null) {
-//                    String videoTitlesString = "";
-//                    boolean addNewLine = false;
-//                    for (Video video : videoList.getData()) {
-//                        if (addNewLine) {
-//                            videoTitlesString += "\n";
-//                        }
-//                        addNewLine = true;
-//                        videoTitlesString += video.mName;
-//                    }
-//                    mRequestOutputTv.setText(videoTitlesString);
-//                }
-//                mStaffPicksRequestTime.setText(getString(R.string.request_time, "Gson", totalLoadTime));
-//
-//                toast("Staff Picks Success ");
-//                mProgressDialog.hide();
-//            }
-//
-//            @Override
-//            public void failure(VimeoError error) {
-//                toast("Staff Picks Failure");
-//                mRequestOutputTv.setText(error.getDeveloperMessage());
-//                mProgressDialog.hide();
-//            }
-//        });
-    }
-
-    private void fetchStaffPicksWithMoshi() {
-        final long initialTime = System.currentTimeMillis();
-        mProgressDialog.show();
-//        mApiClient.getContent(STAFF_PICKS_VIDEO_URI, CacheControl.FORCE_NETWORK, MoshiGetRequestCaller.VIDEO_LIST, null, null, null, new VimeoCallback<com.vimeo.networking2.VideoList>() {
-//            @Override
-//            public void success(com.vimeo.networking2.VideoList videoList) {
-//                final long finalTime = System.currentTimeMillis();
-//                final long totalLoadTime = finalTime - initialTime;
-//
-//                if (videoList != null && videoList.getData() != null) {
-//                    String videoTitlesString = "";
-//                    boolean addNewLine = false;
-//                    for (com.vimeo.networking2.Video video : videoList.getData()) {
-//                        if (addNewLine) {
-//                            videoTitlesString += "\n";
-//                        }
-//                        addNewLine = true;
-//                        videoTitlesString += video.getName();
-//                    }
-//                    mRequestOutputTv.setText(videoTitlesString);
-//                }
-//                mStaffPicksRequestTime.setText(getString(R.string.request_time, "Moshi", totalLoadTime));
-//                toast("Staff Picks Success");
-//                mProgressDialog.hide();
-//            }
-//
-//            @Override
-//            public void failure(VimeoError error) {
-//                toast("Staff Picks Failure");
-//                mRequestOutputTv.setText(error.getDeveloperMessage());
-//                mProgressDialog.hide();
-//            }
-//        });
-    }
-
-    private void fetchAccountType() {
-        mProgressDialog.show();
-//        mApiClient.getCurrentUser(new VimeoCallback<User>() {
-//            @Override
-//            public void success(User user) {
-//                if (user != null) {
-//                    mRequestOutputTv.setText("Current account type: " + user.getMembership().getDisplay());
-//                    toast("Account Check Success");
-//                } else {
-//                    toast("Account Check Failure");
-//                }
-//                mProgressDialog.hide();
-//            }
-//
-//            @Override
-//            public void failure(VimeoError error) {
-//                toast("Account Check Failure");
-//                mRequestOutputTv.setText(error.getDeveloperMessage());
-//                mProgressDialog.hide();
-//            }
-//        });
-    }
-
-    private void logout() {
-        mProgressDialog.show();
-//        mApiClient.logOut(new VimeoCallback<Object>() {
-//            @Override
-//            public void success(Object o) {
-//                AccountPreferenceManager.removeClientAccount();
-//                toast("Logout Success");
-//                mProgressDialog.hide();
-//            }
-//
-//            @Override
-//            public void failure(VimeoError error) {
-//                AccountPreferenceManager.removeClientAccount();
-//                toast("Logout Failure");
-//                mRequestOutputTv.setText(error.getDeveloperMessage());
-//                mProgressDialog.hide();
-//            }
-//        });
-    }
-    // </editor-fold>
-
-    // <editor-fold desc="Code Grant">
-
-
-    private void goToWebForCodeGrantAuth() {
-//        String uri = mApiClient.getCodeGrantAuthorizationURI();
-//        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
-//        startActivity(browserIntent);
-    }
-
-    // </editor-fold>
-
-    // <editor-fold desc="Helpers">
-
-    private void toast(String string) {
-        Toast.makeText(getContext(), string, Toast.LENGTH_SHORT).show();
-    }
-
 
     @Override
     public void onPause() {
